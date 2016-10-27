c3089148477fe1bcef2f7005558a88bf15611cfa
xomin1024 <mx5r8@mail.umkc.edu>
2016-10-26 20:42:58 -0600

    Store data

---
+++ Project2B/store.h
@@ -0,0 +1,98 @@
+#pragma once
+//struct Rating
+//{
+//	int custmer_ID;
+//	int rating;
+//	int book_ID;
+//
+//}
+//
+//vector<Rating>list_rating;
+
+#include<store.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include<map>
+using namespace std;
+
+
+int main()
+{
+	// File I/O
+	ifstream fin("books.txt");
+	ofstream fout("books_output.txt");
+	ifstream fin("customers.txt");
+	ofstream fout("customers_output.txt");
+	ifstream fin("ratings.txt");
+	ofstream fout("ratings_output.txt");
+
+
+	map<string, string> books; // h
+	string book_title; // h
+	string ISBN; // h
+
+	map<int, string> customers; //h
+	int ID; // h
+	string Name; // h
+	map<string, string>::iterator V;// h
+	map<int, string>::iterator K;// h
+
+								 // read in isbn and book title from the file and store it into map
+								 // can put it into separate function and call it when you need to store data (cpp)
+								 // need to ignore commas
+	while (fin >> ISBN >> book_title)
+	{
+
+		books[ISBN] = book_title;
+
+	}
+	//function that finds book title by isbn number, if it is not there it will return Error (cpp)
+	string find_a_title_by_isbn(string new_isbn)
+	{
+
+		for (map<string, string>::iterator V = books.begin(); V != books.end(); V++)
+		{
+			if (V->first == new_isbn)
+			{
+				return V->second;
+
+			}
+			return "Error";
+		}
+	}
+
+}
+// can make it a function to read in id and names from the file (cpp)
+while (fin >> ID >> Name)
+{
+	customers[ID] = Name;
+
+}
+//function that finds book title by isbn number, if it is not there it will return Error (cpp)
+string find_name_by_id(int new_id)
+{
+
+	for (map<int, string>::iterator K = customers.begin(); K != customers.end(); K++)
+	{
+		if (K->first == new_id)
+		{
+			return K->second;
+
+		}
+		return "Error";
+	}
+}
+
+
+/*for (map<string, int> ::iterator V = m2.begin(); V != m2.end(); V++)
+{
+fout << V->first << ",  " << V->second << endl;
+}
+system("pause");
+return 0;*/
+}
+
+
+
+
