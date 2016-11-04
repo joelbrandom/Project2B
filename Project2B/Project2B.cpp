50222b80806af9bdbeaee57368e4f96b2e136307
Joel Brandom <afro.hj@gmail.com>
2016-11-03 21:30:58 -0500

    Functions to populate ratingsVector and maps

--- Project2B/Project2B.cpp
+++ Project2B/Project2B.cpp
@@ -1,9 +1,20 @@
 //#include "stdafx.h"
 #include "store.h"
 #include <iostream>
+#include <map>
+#include <string>
 
 int main()
 {
-  std::cout << "test\n";
+  populateRatingVector("/Users/joel/Documents/School/UMKC Fall 2016/Data Structures/Projects/Project2B/Project2B/ratings.txt");
+  populateMap(booksMap, "/Users/joel/Documents/School/UMKC Fall 2016/Data Structures/Projects/Project2B/Project2B/books.txt");
+  populateMap(customersMap, "/Users/joel/Documents/School/UMKC Fall 2016/Data Structures/Projects/Project2B/Project2B/customers.txt");
+
+  //for (int i = 0; i < ratingVector.size() / 2; ++i)
+    //std::cout << ratingVector[i].customer_ID << ' ' << ratingVector[i].rating << ' ' << ratingVector[i].book_ID << '\n';
+
+  for (std::map<int, std::string>::iterator i = customersMap.begin(); i != customersMap.end(); ++i)
+    std::cout << i->first << ' ' << i->second << '\n';
+
   return 0;
 }
--- Project2B/store.h
+++ Project2B/store.h
@@ -7,15 +7,96 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <regex>
+//#include <omp.h>
 using namespace std;
 
 struct Rating
 {
-	int custmer_ID;
+	int customer_ID;
 	int rating;
 	int book_ID;
 
+	Rating (int custID, int rate, int bookID)
+	{
+		customer_ID = custID;
+		rating = rate;
+		book_ID = bookID;
+	}
+	Rating ()
+	{
+	}
 };
+
+vector<Rating> ratingVector;
+map<int, string> booksMap, customersMap;
+
+// Store ratings into a vector
+void populateRatingVector(const string& source)
+{
+	ifstream ifs(source);
+	if (ifs)
+	{
+		int cID, user_rating, book;
+		string line;
+
+		while (getline(ifs, line))
+		{
+			regex re("([0-9]+), ([0-9]+), ([0-9]+)");
+			smatch match;
+			if (regex_search(line, match, re) && match.size() > 1)
+			{
+				cID = stoi(match.str(1));
+				user_rating = stoi(match.str(2));
+				book = stoi(match.str(3));
+				Rating rating(cID, user_rating, book);
+				ratingVector.push_back(rating);
+			}
+		}
+		ifs.close();
+	}
+}
+
+// Generalized function to populate maps
+// Works for customersMap and booksMap
+void populateMap(map<int, string>& map, const string& source)
+{
+	ifstream ifs(source);
+	if (ifs)
+	{
+		int id;
+		string name, line;
+
+		while (getline(ifs, line))
+		{
+			regex re("([0-9]+), (.*)");
+			smatch match;
+			if (regex_search(line, match, re) && match.size() > 1)
+			{
+				id = stoi(match.str(1));
+				name = match.str(2);
+				map[id] = name;
+			}
+		}
+		ifs.close();
+	}
+}
+
+/*
+void ppp()
+{
+	omp_set_num_threads(3);
+	int MAX = 12, count = 0;
+	#pragma omp parallel for
+	for (int i = 0; i < MAX; ++i)
+	{
+		#pragma omp atomic
+			++count;
+	}
+	std::cout << count << '\n';
+}
+*/
+
 
 /*
 	Below are the beginnings of possible functions to be used
@@ -74,6 +155,8 @@ map<string, string>::iterator V;// h
 map<int, string>::iterator K;// h
 
 //function that finds book title by isbn number, if it is not there it will return Error (cpp)
+// You can find the second from a map using:
+// map_variable["ISBN"]
 string find_a_title_by_isbn(string new_isbn)
 {
 
